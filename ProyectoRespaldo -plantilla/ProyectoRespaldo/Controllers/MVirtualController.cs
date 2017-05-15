cba0801e9be9a9942bc0d06edb2bdb8ef7c54254
Jose Luis Ríos <masther-king@hotmail.com>
2017-05-15 11:05:05 -0500

    Proyecto_Respaldo
    Este proyecto tiene la finalidad de mostrar como es el proceso para
    llevar a cabo un respaldo aplicando las buenas practicas

---
+++ ProyectoRespaldo -plantilla/ProyectoRespaldo/Controllers/MVirtualController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoRespaldo.Models;
+
+namespace ProyectoRespaldo.Controllers
+{
+    public class MVirtualController : Controller
+    {
+        private RespaldoEntities db = new RespaldoEntities();
+
+    
+        // GET: MVirtual
+        public ActionResult Index_MVirtual()
+        {
+            return View(db.persona.ToList());
+        }
+
+
+
+        // GET: MVirtual/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: MVirtual/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MVirtual/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MVirtual/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: MVirtual/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MVirtual/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: MVirtual/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ProyectoRespaldo -plantilla/ProyectoRespaldo/Controllers/PoliticaController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoRespaldo.Controllers
+{
+    public class PoliticaController : Controller
+    {
+        // GET: Politica
+        public ActionResult Index_Politica()
+        {
+            return View();
+        }
+
+        // GET: Politica/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Politica/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Politica/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Politica/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Politica/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Politica/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Politica/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
